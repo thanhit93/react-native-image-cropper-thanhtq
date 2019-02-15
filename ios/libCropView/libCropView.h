05b134796e39eb346cba4acecd7814310284ab19
Trương Thành <>
2019-02-15 20:07:46 +0700

    add file .a

---
+++ ios/libCropView/libCropView.h
@@ -0,0 +1,13 @@
+//
+//  libCropView.h
+//  libCropView
+//
+//  Created by Trương Thành on 2/15/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libCropView : NSObject
+
+@end
---
+++ ios/libCropView/libCropView.m
@@ -0,0 +1,13 @@
+//
+//  libCropView.m
+//  libCropView
+//
+//  Created by Trương Thành on 2/15/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "libCropView.h"
+
+@implementation libCropView
+
+@end
