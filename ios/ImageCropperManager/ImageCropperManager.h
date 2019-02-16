573ac15086111a379fc3f7d582400c47d8d0d552
Trương Thành <>
2019-02-16 16:45:06 +0700

    update target

---
+++ ios/ImageCropperManager/ImageCropperManager.h
@@ -0,0 +1,18 @@
+//
+//  ImageCroperManager.h
+//  TOCropViewControllerExample
+//
+//  Created by Trương Thành on 2/15/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageCropperManager : UINavigationController <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
